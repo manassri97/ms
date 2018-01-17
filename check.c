b6f3460a692bd9dd150816fa16363933f4249c0c
manassri97 <35536041+manassri97@users.noreply.github.com>
2018-01-17 22:29:30 +0530

    Add files via upload

---
+++ check.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the no. to check\n");
+scanf("%d\n",&n);
+if(n<0)
+printf("negative");
+if(n>0)
+printf("positive");
+else
+printf("zero");
+}
